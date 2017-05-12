e48b5066878323cf6a5074406755293dbc6f3634
Torben Knerr <mail@tknerr.de>
2017-05-13 01:04:36 +0200

    switch to eclipse neon j2ee edition which is better suited for developing a web application

--- cookbooks/vm/recipes/eclipse.rb
+++ cookbooks/vm/recipes/eclipse.rb
@@ -6,7 +6,7 @@
 ark 'install eclipse' do
   name 'eclipse'
   version 'neon'
-  url 'http://ftp.heanet.ie/pub/eclipse/technology/epp/downloads/release/neon/R/eclipse-java-neon-R-linux-gtk-x86_64.tar.gz'
-  checksum 'ca06e10df42eab462f130aa89658f9c75f1df61ba79ede02fb7bb748f4544c34'
+  url 'http://ftp.heanet.ie/pub/eclipse/technology/epp/downloads/release/neon/R/eclipse-jee-neon-R-linux-gtk-x86_64.tar.gz'
+  checksum '3a97e2b9e99b09cb6b18bc22fab05977838618999fe62051e52352ae9f00b8f1'
   has_binaries ['eclipse']
 end
