658ed305a6662ff56f7c06d180de584c8c531095
Torben Knerr <mail@tknerr.de>
2017-05-12 15:34:26 +0200

    download, extract and install / symlink maven

--- cookbooks/vm/recipes/default.rb
+++ cookbooks/vm/recipes/default.rb
@@ -7,3 +7,4 @@
 include_recipe 'vm::base'
 include_recipe 'vm::vim'
 include_recipe 'vm::java'
+include_recipe 'vm::maven'
---
+++ cookbooks/vm/recipes/maven.rb
@@ -0,0 +1,22 @@
+
+#
+# install maven
+#
+mvn_version = '3.5.0'
+mvn_checksum = 'beb91419245395bd69a4a6edad5ca3ec1a8b64e41457672dc687c173a495f034'
+
+remote_file 'download maven tarball' do
+  source "http://apache.mirrors.tds.net/maven/maven-3/#{mvn_version}/binaries/apache-maven-#{mvn_version}-bin.tar.gz"
+  checksum mvn_checksum
+  path "#{Chef::Config[:file_cache_path]}/maven-#{mvn_version}.tar.gz"
+end
+
+execute 'extract maven tarball' do
+  command "tar xzvf #{Chef::Config[:file_cache_path]}/maven-#{mvn_version}.tar.gz"
+  cwd "/usr/local"
+  creates "/usr/local/apache-maven-#{mvn_version}/bin/mvn"
+end
+
+link '/usr/local/maven' do
+  to "/usr/local/apache-maven-#{mvn_version}"
+end
